cb2422b4576dab7bb3fba67f854eb27ee3646282
Adrian Wix <adrianwix18@gmail.com>
2018-09-11 19:04:53 +0200

    Ended

--- server.js
+++ server.js
@@ -2,31 +2,43 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/views/index.html");
 });
 
+// your first API endpoint...
+app.get("/api/timestamp/:timestamp?", function(req, res) {
+    let time = req.params.timestamp;
+    let date;
+    console.log(time);
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+    if (!time) {
+        date = new Date();
+    } else if (!isNaN(time)) {
+        date = new Date(time * 1000);
+    } else {
+        date = new Date(time);
+    }
+
+    res.json({
+        unix: date.getTime(),
+        utc: date.toUTCString()
+    });
 });
-
-
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+var listener = app.listen(5000, function() {
+    console.log("Your app is listening on port " + listener.address().port);
 });
